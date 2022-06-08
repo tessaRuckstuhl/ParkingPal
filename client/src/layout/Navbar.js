7042069db8541ee5f83aa27c9e5b5d7cfb195dd9
ge56fis <ge56fis@mytum.de>
2022-06-08 14:48:05 +0200

    merge face and menu into one button

--- client/src/layout/Navbar.js
+++ client/src/layout/Navbar.js
@@ -7,11 +7,9 @@ import { IconButton } from '@mui/material';
 const Navbar = () => {
   return (
     <div className="flex justify-between py-2 px-5">
-      <div className="w-6 h-6">
         <Link to="dashboard">
-          <img src="./parkingpal-logo.png" alt="Parkingpal logo"></img>
+          <img src="./parkingpal-logo.png" width={24} height={24} alt="Parkingpal logo"></img>
         </Link>
-      </div>
       <div className="flex space-x-4 items-center">
         <Link to="404">
           <div className="text-xs">Become a host</div>
@@ -19,11 +17,9 @@ const Navbar = () => {
         <IconButton onClick={() => alert('TBD')}>
           <LanguageIcon sx={{ fontSize: 20 }} color="secondary" />
         </IconButton>
-        <div className="flex space-x-1 border-darkGray rounded-3xl border px-2">
-          <IconButton onClick={() => alert('TBD')}>
+        <div className="flex space-x-1 border-darkGray rounded-3xl border px-1">
+          <IconButton onClick={() => alert('TBD')} className='flex space-x-1'>
             <MenuIcon sx={{ fontSize: 20 }} color="secondary" />
-          </IconButton>
-          <IconButton onClick={() => alert('TBD')}>
             <FaceIcon sx={{ fontSize: 20 }} color="secondary" />
           </IconButton>
         </div>
