2f5f0daf31b10b7f1a4a45a485fe24b75afc6ba1
Jakob Kempter <jakob.kempter@inovex.de>
2022-06-21 15:58:19 +0200

    Minor changes, need to wait until sb solves my problem on his/her branch

--- client/src/components/Forms/ReviewForm.js
+++ client/src/components/Forms/ReviewForm.js
@@ -66,6 +66,8 @@ const ReviewForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      const { id } = this.props.match.params // read id from url params
+
       const user = parseJwt(localStorage.getItem('token'))
       const review = {
         description: reviewDescription,
@@ -76,8 +78,8 @@ const ReviewForm = () => {
         accuracyRating: accuracyRating,
         valueRating: valueRating,
 
-        owner: user,
-        parkingSpace_id: "Mock"// How do I geht this value?
+        owner: user,        // Ich schreib hier gerade den kompletten user object rein, reicht hier ein String?
+        parkingSpace_id: id // nur die id als string
       };
       const response = await ReviewService.create(review);
     } catch (error) {
@@ -94,8 +96,16 @@ const ReviewForm = () => {
 
   useEffect(() => {
 
+
+    const { token, email } = this.props.match.params
+    // Wird beim Rendern der Seite ausgeführt 
+    // hier Id von den params auslesen. 
+    const parkingspace_id = 234823492349
+    // Wie kann ich darauf zugreifen?
+
   }, []);
   return (
+    
     <div className="flex flex-col items-center ">
       <div className="w-3/4">
         <div className="mb-6 text-xl">
@@ -103,155 +113,155 @@ const ReviewForm = () => {
         </div>
 
         <form className="text-3x2 font-bold mb-7" noValidate onSubmit={(e) => handleSubmit(e)}>
-        <div className="mb-6 text-xl">
-          <p>Your parking space booking came just to an end.<br />Now take a minute to reflect on the parking place and leave a quick review. </p>
-        </div>
-        <Divider>
-
-        </Divider>
-       
-        
-        <div>
-          <p>You booked this parking place provides by<br />Alexander Mock. Adresse Platzhalter. Willkürliche Zeitdauer </p>
-
-          <p>Here is a short summary of what was promised and expected</p>
-
-          <b>Placeholder Booking summary</b>
-        </div>
-        <Divider>
-
-        </Divider>
-
-        <div>
           <div className="mb-6 text-xl">
-            <b>Rate your experience</b> <br />
-            <p> Please rate your parking place experience in overall satisfaction, accesability, service by the host and location</p>
+            <p>Your parking space booking came just to an end.<br />Now take a minute to reflect on the parking place and leave a quick review. </p>
           </div>
-          <Grid container spacing={3}>
-            <Grid item xs={4}>
-              <Item>Neighborhood</Item>
-              <Rating
-                defaultValue={2.5}
-                precision={0.5}
-                variant="outlined"
-                required
-                id="neighborhoodrating"
-                label="Neighbor Rating"
-                name="neighborhoodrating"
-                value={neighborhoodRating}
-                autoFocus
-                onChange={(e) => handleChange(e)}
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <Item>Access</Item>
-              <Rating
-                defaultValue={2.5}
-                precision={0.5}
-                variant="outlined"
-                required
-                id="accessrating"
-                label="Access Rating"
-                name="accessrating"
-                value={accessRating}
-                autoFocus
-                onChange={(e) => handleChange(e)}
-              />            </Grid>
-            <Grid item xs={4}>
-              <Item>Location</Item>
-              <Rating
-                defaultValue={2.5}
-                precision={0.5}
-                variant="outlined"
-                required
-                id="locationrating"
-                label="Location Rating"
-                name="locationrating"
-                value={locationRating}
-                autoFocus
-                onChange={(e) => handleChange(e)}
-              />
-            </Grid>
-          </Grid>
-          <Grid container spacing={3}>
-            <Grid item xs={4}>
-              <Item>Communication</Item>
-              <Rating
-                defaultValue={2.5}
-                precision={0.5}
-                variant="outlined"
-                required
-                id="communicationrating"
-                label="Communication Rating"
-                name="communicationrating"
-                value={communicationRating}
-                autoFocus
-                onChange={(e) => handleChange(e)}
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <Item>Accuracy</Item>
-              <Rating
-                defaultValue={2.5}
-                precision={0.5}
-                variant="outlined"
-                required
-                id="accuracyrating"
-                label="Accuracy Rating"
-                name="accuracyrating"
-                value={accuracyRating}
-                autoFocus
-                onChange={(e) => handleChange(e)}
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <Item>Value</Item>
-              <Rating
-                defaultValue={2.5}
-                precision={0.5}
-                variant="outlined"
-                required
-                id="valuerating"
-                label="Value Rating"
-                name="valuerating"
-                value={valueRating}
-                autoFocus
-                onChange={(e) => handleChange(e)}
-              />
-            </Grid>
-          </Grid>
-        </div>
-        <Divider>
+          <Divider>
 
-        </Divider>
-        <div>
-          <div className="mb-6 text-xl">
-            <b>Describe your experience</b>
-            <p> Help others by describing your experience</p>
+          </Divider>
+
+
+          <div>
+            <p>You booked this parking place provides by<br />Alexander Mock. Adresse Platzhalter. Willkürliche Zeitdauer </p>
+
+            <p>Here is a short summary of what was promised and expected</p>
+
+            <b>Placeholder Booking summary</b>
           </div>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="reviewdescription"
-            label="Review Description"
-            name="reviewdescription"
-            style={{ height: 100 }}
-            value={reviewDescription}
-            autoFocus
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-        >
-          Submit Review
-        </Button>
+          <Divider>
+
+          </Divider>
+
+          <div>
+            <div className="mb-6 text-xl">
+              <b>Rate your experience</b> <br />
+              <p> Please rate your parking place experience in overall satisfaction, accesability, service by the host and location</p>
+            </div>
+            <Grid container spacing={3}>
+              <Grid item xs={4}>
+                <Item>Neighborhood</Item>
+                <Rating
+                  defaultValue={2.5}
+                  precision={0.5}
+                  variant="outlined"
+                  required
+                  id="neighborhoodrating"
+                  label="Neighbor Rating"
+                  name="neighborhoodrating"
+                  value={neighborhoodRating}
+                  autoFocus
+                  onChange={(e) => handleChange(e)}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <Item>Access</Item>
+                <Rating
+                  defaultValue={2.5}
+                  precision={0.5}
+                  variant="outlined"
+                  required
+                  id="accessrating"
+                  label="Access Rating"
+                  name="accessrating"
+                  value={accessRating}
+                  autoFocus
+                  onChange={(e) => handleChange(e)}
+                />            </Grid>
+              <Grid item xs={4}>
+                <Item>Location</Item>
+                <Rating
+                  defaultValue={2.5}
+                  precision={0.5}
+                  variant="outlined"
+                  required
+                  id="locationrating"
+                  label="Location Rating"
+                  name="locationrating"
+                  value={locationRating}
+                  autoFocus
+                  onChange={(e) => handleChange(e)}
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+              <Grid item xs={4}>
+                <Item>Communication</Item>
+                <Rating
+                  defaultValue={2.5}
+                  precision={0.5}
+                  variant="outlined"
+                  required
+                  id="communicationrating"
+                  label="Communication Rating"
+                  name="communicationrating"
+                  value={communicationRating}
+                  autoFocus
+                  onChange={(e) => handleChange(e)}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <Item>Accuracy</Item>
+                <Rating
+                  defaultValue={2.5}
+                  precision={0.5}
+                  variant="outlined"
+                  required
+                  id="accuracyrating"
+                  label="Accuracy Rating"
+                  name="accuracyrating"
+                  value={accuracyRating}
+                  autoFocus
+                  onChange={(e) => handleChange(e)}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <Item>Value</Item>
+                <Rating
+                  defaultValue={2.5}
+                  precision={0.5}
+                  variant="outlined"
+                  required
+                  id="valuerating"
+                  label="Value Rating"
+                  name="valuerating"
+                  value={valueRating}
+                  autoFocus
+                  onChange={(e) => handleChange(e)}
+                />
+              </Grid>
+            </Grid>
+          </div>
+          <Divider>
+
+          </Divider>
+          <div>
+            <div className="mb-6 text-xl">
+              <b>Describe your experience</b>
+              <p> Help others by describing your experience</p>
+            </div>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="reviewdescription"
+              label="Review Description"
+              name="reviewdescription"
+              style={{ height: 100 }}
+              value={reviewDescription}
+              autoFocus
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            Submit Review
+          </Button>
         </form>
-        
+
 
       </div>
     </div>
@@ -259,3 +269,6 @@ const ReviewForm = () => {
 };
 
 export default ReviewForm;
+
+
+// id in den url params mitgegeben 
