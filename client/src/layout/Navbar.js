c51df7c24bb013d4a4d72b181d0c233362b06d43
ga27jip <ga27jip@mytum.de>
2022-07-12 14:03:37 +0200

    basic account menu

--- client/src/layout/Navbar.js
+++ client/src/layout/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import * as React from 'react';
 import FaceIcon from '@mui/icons-material/Face';
 import MenuIcon from '@mui/icons-material/Menu';
 import LanguageIcon from '@mui/icons-material/Language';
@@ -6,10 +6,21 @@ import { Link } from 'react-router-dom';
 import { IconButton } from '@mui/material';
 import Bar from '../components/Filter/Bar';
 import { useLocation } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const location = useLocation();
-  const [showFilters, setShowFilters] = useState(false)
+  const navigate = useNavigate();
+  const [showFilters, setShowFilters] = React.useState(false)
    const links = []
   // [{ name: "HOME", link: "/dashboard" },
   // { name: "Login", link: "/login" },
@@ -17,9 +28,17 @@ const Navbar = () => {
   // { name: "Review", link: "/parking/review" },
   // { name: "Create Parking", link: "/parking/create" },
   // ];
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
 
-  useEffect(() => {
+  React.useEffect(() => {
     if(location.pathname == "/"){
       setShowFilters(true)
     } else {
@@ -37,24 +56,85 @@ const Navbar = () => {
         {showFilters && <Bar/>}
         {/* Language, Profile, .. */}
       <div className="flex space-x-2 items-center">
-      <Link to="/login">
-          <div className="text-xs">LOGIN</div>
-        </Link>
-        <Link to="/dashboard">
-          <div className="text-xs">DASH/LOGOUT</div>
-        </Link>
-        <Link to="404">
-          <div className="text-xs">Become a host</div>
-        </Link>
         <IconButton onClick={() => alert('TBD')}>
           <LanguageIcon sx={{ fontSize: 20 }} color="secondary" />
         </IconButton>
         <div className="flex space-x-1 border-darkGray rounded-3xl border px-1">
-          <IconButton onClick={() => alert('TBD')} className='flex space-x-1'>
+          <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
             <MenuIcon sx={{ fontSize: 20 }} color="secondary" />
             <FaceIcon sx={{ fontSize: 20 }} color="secondary" />
           </IconButton>
-        </div>
+        </Tooltip>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem onClick={() => navigate('/dashboard')}>
+          <Avatar/> Profile
+        </MenuItem>
+        <MenuItem>
+          <Avatar /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+      </div>
       <ul className="md:flex md:items-center">
         {links.map(link =>(
           <li key= {link.name} className="md:ml-8 text">
